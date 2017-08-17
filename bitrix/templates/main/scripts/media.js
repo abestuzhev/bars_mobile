58ea7bd00932d5337bea84012aeec8879b65654f
abestuzhev <aleksey.bestuzhev@gmail.com>
2017-08-17 12:35:34 +0400

    changes

--- bitrix/templates/main/scripts/media.js
+++ bitrix/templates/main/scripts/media.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // $('.header-top_mobile-menu').on('click', function(e){
     //     $('.sidebar-left').toggle();
     //     e.preventDefault();
@@ -10,22 +10,29 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
-    $('.items_mobile-filter').on('click', function(e){
+    $('.items_mobile-filter').on('click', function (e) {
         $('.side-filter').addClass('filter-active');
         $('.mobile_filter-bg').addClass('filter-active');
         $('body').addClass('mobile-canvas');
         e.preventDefault();
     });
 
-    $('.mobile_filter-bg').on('click', function(e){
+    $('.mobile_filter-bg').on('click', function (e) {
         $('.side-filter').removeClass('filter-active');
         $(this).removeClass('filter-active');
         $('body').removeClass('mobile-canvas');
         e.preventDefault();
     });
 
-    $('.-tree-node').on('click', function(e){
-        console.log('клик рабоатет');
+    $('.header-cart-box').click(function () {
+        window.location.assign('/personal/basket/');
     });
 
+    /*3й уровень меню*/
+    $('.menu-2-level').on('click', function(e){
+        $(this).toggleClass('sub-panel--active');
+        e.preventDefault();
+        $(this).siblings('.menu-2-level').removeClass('sub-panel--active');
+    });
 });
+/* End */
